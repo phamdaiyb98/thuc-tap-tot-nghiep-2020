ddea7ff65f66c4e2263a0240cafaa63eaf29074b
namtao <vanam.pxhn@gmail.com>
2020-04-11 08:40:03 +0700

    fb

---
+++ MobileShop/src/main/java/haui/mobileshop/global/entity/Data.java
@@ -0,0 +1,16 @@
+package haui.mobileshop.global.entity;
+
+public class Data {
+    AccessTokenData data;
+
+    public Data() {
+    }
+
+    public AccessTokenData getData() {
+        return data;
+    }
+
+    public void setData(AccessTokenData data) {
+        this.data = data;
+    }
+}
